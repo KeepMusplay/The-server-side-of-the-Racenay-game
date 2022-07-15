8c28b9ccbe828fee9a0a03d055fcc73b910342cc
Vadim <86560803+KeepMusplay@users.noreply.github.com>
2022-07-15 17:19:09 +0300

    Add files via upload

---
+++ ServerRacenay/CloserConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Dedicate_Server_Unity3d_LastVersion
+{
+    internal class CloserConnection
+    {
+        public static void Close(MySqlConnection conn, int ms)
+        {
+            Thread.Sleep(ms);
+
+
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
